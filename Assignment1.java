74fa09465430de916d674527863374e2cbb16344
SkylabMak <skylab135@gmail.com>
2023-12-11 11:21:28 +0700

    drawSquare

--- Assignment1.java
+++ Assignment1.java
@@ -15,6 +15,7 @@ public class Assignment1 extends JPanel {
         f.setVisible(true);
         f.setLocationRelativeTo(null); // หน้าต่างอยู่ตรงกลาง
     }
+
     @Override
     public void paintComponent(Graphics g) {
         myPaint(g);
@@ -33,7 +34,29 @@ public class Assignment1 extends JPanel {
         }
     }
 
-    private void drawLine(Graphics g, int x1, int y1, int x2, int y2, Color color,int thickness) {
+    private void drawSquare(Graphics g, int x, int y, int x2, int y2, int x3, int y3, int x4, int y4, Color colorO,Color colorI,
+            int thickness) {
+        if(colorO != null){
+drawLine(g, x, y, x2, y2, colorO, thickness);// top
+        drawLine(g, x2, y2, x3, y3, colorO, thickness);// right
+        drawLine(g, x3, y3, x4, y4, colorO, thickness);// crease
+        drawLine(g, x4, y4, x, y, colorO, thickness);// left
+        }
+        
+        if(colorI != null){
+// Fill the inner part of the rectangle
+        int tHalf = thickness /2;
+        g.setColor(colorI);
+        g.fillPolygon(new int[]{x+tHalf,x2-tHalf, x4+tHalf}, new int[]{y+tHalf,y2+tHalf, y4-tHalf},3);
+        g.fillPolygon(new int[]{x2-tHalf,x3-tHalf, x4+tHalf}, new int[]{y2+tHalf,y3-tHalf, y4-tHalf},3);
+        }
+        
+        
+    }
+
+   
+
+    private void drawLine(Graphics g, int x1, int y1, int x2, int y2, Color color, int thickness) {
         g.setColor(color);
         // g.set
         Graphics2D g2d = (Graphics2D) g;
@@ -42,12 +65,8 @@ public class Assignment1 extends JPanel {
         g2d.drawLine(x1, y1, x2, y2);
     }
 
-    private void drawArc(Graphics g, int x, int y, int width, int height, int startAngle, int arcAngle) {
-        g.drawArc(x, y, width, height, startAngle, arcAngle);
-    }
-
-
-    private void drawBezierCurve(Graphics g, int x1, int y1, int ctrlX, int ctrlY, int x2, int y2, int thickness,Color color) {
+    private void drawBezierCurve(Graphics g, int x1, int y1, int ctrlX, int ctrlY, int x2, int y2, int thickness,
+            Color color) {
         g.setColor(color);
         int resolution = 500;
 
@@ -73,12 +92,11 @@ public class Assignment1 extends JPanel {
     }
 
     private void myPaint(Graphics g) {
-        // g.drawPdrawPolygon(g,1200, 1200, 0.5); 
+        // g.drawPdrawPolygon(g,1200, 1200, 0.5);
         // พื้นหลัง
         drawRectangle(g, 0, 0, 600, 600, Color.WHITE);
 
-
-        //test curvature
+        // test curvature
         int x1 = 50;
         int y1 = 200;
         int x2 = 350;
@@ -87,15 +105,45 @@ public class Assignment1 extends JPanel {
         int ctrlX = x1 + 50; // Adjust this value for curvature
         int ctrlY = y1 - 50; // Adjust this value for curvature
 
-        drawBezierCurve(g, x1, y1, ctrlX, ctrlY, x2, y2, 9,Color.BLACK); // Adjust thickness here
-        drawNoteBook(g,50,450);
+        drawBezierCurve(g, x1, y1, ctrlX, ctrlY, x2, y2, 9, Color.BLACK); // Adjust thickness here
+        drawNoteBook(g, 50, 450);
     }
-    void drawNoteBook(Graphics g,int x,int y ){
+
+    void drawNoteBook(Graphics g, int x, int y) {
         // g.setColor(Color.BLACK);
-        drawLine(g, x, y+10, x+50, y, Color.BLACK,3);        
-        drawLine(g, x+50, y, x+55, y+40, Color.BLACK,3);
+       // base - board
+        drawSquare(g, x + 10, y + 50, x + 56, y + 40, x + 80, y + 55, x + 40, y + 70, null,Color.decode("#40476E"), 1);
+         // keyboard
+        drawSquare(g, x + 20, y + 51, x + 55, y + 43, x + 72, y + 52, x + 40, y + 63,Color.black,Color.decode("#82869C"), 1);
+        //keyboard horizon
+        int r = 0;
+        int c = 0;
+        for (int i = 0; i < 26; i+=4) {
+            drawLine(g, x + 23+i, y + 51-r, x + 42+i, y + 63-r-c, Color.BLACK, 1);// top
+            r++;c++;
+        }
+        //keyboard Vertical
+         r = 0;
+         c = 0;
+        for (int i = 0; i < 9; i+=2) {
+            drawLine(g, x + 23+r, y + 51+i, x + 55+r, y + 43+i , Color.BLACK, 1);// top
+            r+=4;c++;
+        }
+        
+        // monitor
+        drawLine(g, x, y + 10, x + 50, y, Color.BLACK, 3);// top
+        drawLine(g, x + 50, y, x + 55, y + 40, Color.BLACK, 3);// right
+        drawLine(g, x + 55, y + 40, x + 10, y + 50, Color.BLACK, 3);// crease
+        drawLine(g, x + 8, y + 50, x, y + 10, Color.BLACK, 4);// left
+        // drawRectangle(g, x, y1, x2, y2, Color.CYAN);
+
+        // base - edge
+        drawLine(g, x + 55, y + 38, x + 82, y + 55, Color.BLACK, 3);// right
+        drawLine(g, x + 9, y + 50, x + 40, y + 70, Color.BLACK, 5);// left
+        drawLine(g, x + 80, y + 55, x + 40, y + 70, Color.BLACK, 5);// button
+        
+       
         
     }
-    
 
 }
